3ad2c5016e67cf4b71c90439823f12c94c5230dc
tonyanyanwu5448 <anthonynzubechukwu5448@gmail.com>
2025-08-02 23:30:37 +0100

    Force Python 3.10 and update TensorFlow

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+# Explicitly use Python 3.10
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install --no-cache-dir -r requirements.txt
