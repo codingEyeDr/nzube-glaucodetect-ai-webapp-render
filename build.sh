44eeff4ad4927c0f593552edb7d3005c97d1e260
tonyanyanwu5448 <anthonynzubechukwu5448@gmail.com>
2025-08-02 23:59:37 +0100

    Force Groq installation

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Explicitly use Python 3.10
 python3.10 -m pip install --upgrade pip
 python3.10 -m pip install --no-cache-dir -r requirements.txt
+python3.10 -m pip install --force-reinstall groq==0.3.0  # Add this line
